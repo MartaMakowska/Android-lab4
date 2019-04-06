36d97b6bd3da1916244af46f9cf7a3223ce32891
marta <marta.martusss@wp.pl>
2019-04-06 16:51:02 +0200

    Create ListView

--- app/src/main/java/com/example/android_lab4/MainActivity.java
+++ app/src/main/java/com/example/android_lab4/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.android_lab4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ArrayList<String> target;
+    private ArrayAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String[] values=new String[]{"Pies","Kot","Koń","Gołąb","Kruk","Dzik","Karp","Osioł","Chomik","Mysz","Jeż"};
+        this.target=new ArrayList<String>();
+        this.target.addAll(Arrays.asList(values));
+        this.adapter=new ArrayAdapter(this,android.R.layout.simple_list_item_1,this.target);
+        ListView listView=(ListView)findViewById(android.R.id.list);
+        listView.setAdapter(this.adapter);
+
     }
+
 }
